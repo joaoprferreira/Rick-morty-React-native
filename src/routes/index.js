e21ce27a197a85674bd431e964873c7a1a2f0300
João Pedro <joaof082@gmail.com>
2022-06-05 01:10:05 -0300

    refact: adding function to create stack
    Signed-off-by: João Pedro <joaof082@gmail.com>

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/native-stack';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Home from '../Pages/Home/Home';
+import Details from '../Pages/Details/Details';
 
-const Stack = createStackNavigator();
+const Stack = createNativeStackNavigator();
 
 function Routes() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Details" component={Details} />
       </Stack.Navigator>
     </NavigationContainer>
   );
