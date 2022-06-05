65df4f58174cc151ec602ecc8b3406630bb56626
João Pedro <joaof082@gmail.com>
2022-06-05 01:09:18 -0300

    feat: building component 'Datails'
    Signed-off-by: João Pedro <joaof082@gmail.com>

---
+++ src/Pages/Details/Details.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+
+function Details() {
+  return (
+    <View>
+      <Text>Details</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default Details;
